47a21bb70f7f2bf03fd8c1fe22b7df82c7cf3fa4
arunjain83 <46408587+arunjain83@users.noreply.github.com>
2022-02-17 18:12:21 -0500

    Updated with a specific array of int

--- src/FindDuplicate.java
+++ src/FindDuplicate.java
@@ -1,6 +1,15 @@
 public class FindDuplicate {
 
     public static void main (String[] arg){
-
+        int[] arr = {1,3,4,5,6,8,8};
+        for (int i =0; i<arr.length; i++){
+            for (int j=i+1; j<arr.length; j++){
+                if(arr[i] == arr[j]){
+                    System.out.println("True");
+                    System.exit(0);
+                }
+            }
+        }
+        System.out.println("False");
     }
 }
